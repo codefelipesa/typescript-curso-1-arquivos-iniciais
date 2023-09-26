d13a98e542325811daa4036c6ba7921d47228d09
Felipe <felipesa1411@.com>
2023-09-26 16:56:52 -0300

    npm install TypeScript

--- dist/js/models/negociacao.ts
+++ dist/js/models/negociacao.ts
@@ -12,6 +12,7 @@ export class Negociacao{
 
     get data (){return this.#data}        
     get quantidade (){return this.#quantidade}        
-    get valor (){return this.#valor}        
+    get valor (){return this.#valor} 
+    get volume (){return this.#quantidade * this.#valor}       
 }
 
