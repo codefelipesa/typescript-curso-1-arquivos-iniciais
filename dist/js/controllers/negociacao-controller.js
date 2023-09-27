ba1a603cd79a1352c79b8b548d3188a96881c777
Felipe <felipesa1411@.com>
2023-09-27 16:53:27 -0300

    duplicate on archive ts

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -4,4 +4,9 @@ export class NegociacaoController {
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
+    adiciona() {
+        console.log(this.inputData);
+        console.log(this.inputQuantidade);
+        console.log(this.inputValor);
+    }
 }
