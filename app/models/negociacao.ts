52d5fa7b7b823f05c2e50404c9a060acd126a2e4
Felipe <felipesa1411@.com>
2023-09-27 14:15:51 -0300

    add private propriority

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,18 +1,18 @@
 export class Negociacao{
-    #data;
-    #quantidade;
-    #valor;
+    private _data;  // Usamos o atributo private para declaração de valor privado, e o "_" para não ter nomes duplicados
+    private _quantidade;  // Usamos o atributo private para declaração de valor privado, e o "_" para não ter nomes duplicados
+    private _valor;  // Usamos o atributo private para declaração de valor privado, e o "_" para não ter nomes duplicados
 
     constructor(data, quantidade, valor){
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
 
     }
 
-    get data (){return this.#data}        
-    get quantidade (){return this.#quantidade}        
-    get valor (){return this.#valor} 
-    get volume (){return this.#quantidade * this.#valor}       
+    get data (){return this._data}        
+    get quantidade (){return this._quantidade}        
+    get valor (){return this._valor} 
+    get volume (){return this._quantidade * this._valor}       
 }
 
