d91d3b0ec1dde88d3f8e8628ff3df1c99644e51e
Felipe <felipesa1411@.com>
2023-09-27 17:20:57 -0300

    add getForm

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,9 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100)
-console.log(negociacao)
+const controller = new NegociacaoController()
+
+const getForm = document.querySelector('[data-form]')
+getForm.addEventListener('submit', e => {
+    e.preventDefault()
+    controller.adiciona()
+}) 
