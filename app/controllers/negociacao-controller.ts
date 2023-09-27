ecf6a04e55ed3a375e04f0da39370dc1c5016138
Felipe <felipesa1411@.com>
2023-09-27 16:53:06 -0300

    creater function adiciona()

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -8,4 +8,10 @@ export class NegociacaoController {
         this.inputQuantidade = document.querySelector('#quantidade')
         this.inputValor = document.querySelector('#valor')
     }
+
+    adiciona(){
+        console.log(this.inputData)
+        console.log(this.inputQuantidade)
+        console.log(this.inputValor)
+    }
 }
