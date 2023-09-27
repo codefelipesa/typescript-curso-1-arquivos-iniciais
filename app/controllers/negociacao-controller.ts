27a808445c894be4fc02f335a75a8d5ea706c3bc
Felipe <felipesa1411@.com>
2023-09-27 15:35:10 -0300

    Criação da class NegociacaoController

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,11 @@
+export class NegociacaoController {
+    private inputData
+    private inputQuantidade
+    private inputValor
+
+    constructor(){
+        this.inputData = document.querySelector('#data')
+        this.inputQuantidade = document.querySelector('#quantidade')
+        this.inputValor = document.querySelector('#valor')
+    }
+}
---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,7 @@
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+}
