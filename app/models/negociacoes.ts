e0d18019a2d80ffa018d3ccab1cd5433bb7201e7
Felipe <felipesa1411@.com>
2023-09-28 16:33:45 -0300

    add lista

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,5 +1,16 @@
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes {
-private negociacoes :Array<Negociacao> = [] // Declaramos o tipo de "coisas" que o Array vai receber
+    export class Negociacoes {
+    private negociacoes :Array<Negociacao> = [] // Declaramos o tipo de "coisas" que o Array vai receber
+    
+    adiciona(negociacao : Negociacao){
+        this.negociacoes.push(negociacao)
+    }
+
+    lista(): Array <Negociacao> {
+        return this.negociacoes
+    }
 }
+
+const negociacoes = new Negociacoes()
+negociacoes.lista().forEach(n => console.log(n))
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,4 +2,12 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = []; // Declaramos o tipo de "coisas" que o Array vai receber
     }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
 }
+const negociacoes = new Negociacoes();
+negociacoes.lista().forEach(n => console.log(n));
