bc2633cd91280f29d3e437e3d51c7261e9b3d371
Felipe <felipesa1411@.com>
2023-09-28 16:33:34 -0300

    add new private

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js"
+import { Negociacoes } from "../models/negociacoes.js"
 
 export class NegociacaoController {
     private inputData :HTMLInputElement
     private inputQuantidade :HTMLInputElement
     private inputValor :HTMLInputElement
+    private negociacoes : Negociacoes = new Negociacoes()
 
     constructor(){
         this.inputData = document.querySelector('#data')
@@ -13,7 +15,6 @@ export class NegociacaoController {
 
     adiciona() : void {
         const negociacao = this.criaNegociacao()
-        console.log(negociacao)
         this.limparForm()
 
     }
