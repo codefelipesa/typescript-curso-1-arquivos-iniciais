d38c6b6807fcd34fbb5baf49d3f6957e7be290f0
Felipe <felipesa1411@.com>
2023-09-28 12:06:40 -0300

    add correct type

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,9 +1,9 @@
 import { Negociacao } from "../models/negociacao.js"
 
 export class NegociacaoController {
-    private inputData
-    private inputQuantidade
-    private inputValor
+    private inputData :HTMLInputElement
+    private inputQuantidade :HTMLInputElement
+    private inputValor :HTMLInputElement
 
     constructor(){
         this.inputData = document.querySelector('#data')
@@ -12,64 +12,7 @@ export class NegociacaoController {
     }
 
     adiciona(){
-        const negociacao = new Negociacao(this.inputData.
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            , this.inputQuantidade.value, this.inputValor.value )
+        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValor.value )
         console.log(negociacao)
     }
 }
