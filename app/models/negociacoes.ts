1265eff23bc26e71eff9f7fb9f73b611f77c1b2a
Felipe <felipesa1411@.com>
2023-09-28 14:55:09 -0300

    create class Negociações

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,5 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+private negociacoes :Array<Negociacao> = [] // Declaramos o tipo de "coisas" que o Array vai receber
+}
---
+++ dist/js/models/negociacoes.js
@@ -0,0 +1,5 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = []; // Declaramos o tipo de "coisas" que o Array vai receber
+    }
+}
