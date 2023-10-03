932345d9aa20b6f3bddd52e8af5561e3bf3064ab
Felipe <felipesa1411@.com>
2023-10-03 15:51:53 -0300

    update code

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from "../models/negociacao.js"
 import { Negociacoes } from "../models/negociacoes.js"
+import { MensagemView } from "../views/mensagem-view.js"
 import { NegociacoesView } from "../views/negociacoes-view.js"
 
 export class NegociacaoController {
@@ -8,18 +9,19 @@ export class NegociacaoController {
     private inputValor :HTMLInputElement
     private negociacoes = new Negociacoes()
     private negociacoesView = new NegociacoesView("[data-negociacoes-views]")
+    private mensagemView = new MensagemView("#mensagemView")
 
     constructor(){
         this.inputData = document.querySelector('#data')
         this.inputQuantidade = document.querySelector('#quantidade')
         this.inputValor = document.querySelector('#valor')
-        this.negociacoesView.update(this.negociacoes)
+        this.negociacoesView.update(this.negociacoes  )
     }
 
     adiciona() : void {
         const negociacao = this.criaNegociacao()
         this.negociacoes.adiciona(negociacao)
-        console.table  (this.negociacoes.lista())
+        this.negociacoesView.update(this.negociacoes)
         this.limparForm()
 
     }
