7f52d7d73c1c169155a183393c1a4b40ef529032
Felipe <felipesa1411@.com>
2023-10-23 14:33:00 -0300

    build:  Move dist for app

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,7 @@ import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController()
 
-const getForm = document.querySelector('[data-form]')
+const getForm = document.querySelector('[data-form]') as HTMLInputElement
 getForm.addEventListener('submit', e => {
     e.preventDefault()
     controller.adiciona()
